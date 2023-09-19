d68c3083e94d444d8f9f9c96e62a37abf2765b2c
Matthew Christopher <89071215+mattchristopher314@users.noreply.github.com>
2023-09-19 11:16:03 +0100

    Add redirect middleware to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,51 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { Connect, defineConfig, ViteDevServer } from "vite";
+import react from "@vitejs/plugin-react";
+import { IncomingMessage, ServerResponse } from "http";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    {
+      name: "rewrite-middleware",
+      configureServer(serve: ViteDevServer) {
+        serve.middlewares.use(
+          (
+            req: IncomingMessage,
+            res: ServerResponse<IncomingMessage>,
+            next: Connect.NextFunction
+          ) => {
+            console.log(req.url);
+            if (req.url.startsWith("/app")) {
+              req.url = "/app/";
+            }
+            next();
+          }
+        );
+      },
+      configurePreviewServer(serve: ViteDevServer) {
+        serve.middlewares.use(
+          (
+            req: IncomingMessage,
+            res: ServerResponse<IncomingMessage>,
+            next: Connect.NextFunction
+          ) => {
+            console.log(req.url);
+            if (req.url.startsWith("/app")) {
+              req.url = "/app/";
+            }
+            next();
+          }
+        );
+      },
+    },
+  ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        app: "app/index.html",
+      },
+    },
+  },
+});
