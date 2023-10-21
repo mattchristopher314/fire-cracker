e9d1ed17211d30fe643df2de272b17d7127fef93
mattchristopher314 <89071215+mattchristopher314@users.noreply.github.com>
2023-10-21 11:02:01 +0100

    Add profile page navigation animation and feedback

--- src/features/authentication/User.tsx
+++ src/features/authentication/User.tsx
@@ -4,6 +4,19 @@ import MiniSpinner from "../../ui/MiniSpinner";
 import { useProfileData } from "../../context/useProfileData";
 import { NavLink } from "react-router-dom";
 
+const UserAvatar = styled.img`
+  display: block;
+  width: 2.6rem;
+  height: 2.6rem;
+  object-fit: cover;
+  object-position: center;
+  border-radius: 50%;
+  outline: 2px solid var(--color-grey-100);
+  transition: outline 0.3s ease-in-out;
+  z-index: 2;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.2);
+`;
+
 const StyledUser = styled(NavLink)`
   margin-left: auto;
   display: flex;
@@ -12,6 +25,70 @@ const StyledUser = styled(NavLink)`
   font-weight: 500;
   font-size: 1.4rem;
   color: var(--color-grey-600);
+
+  &::before,
+  &::after {
+    content: "";
+    background: var(--color-brand-lo);
+    position: absolute;
+    border-radius: 50%;
+
+    transition: transform 0.4s cubic-bezier(0.6, -0.7, 0.4, 1.7);
+  }
+
+  &::before {
+    width: 3rem;
+    height: 3rem;
+    z-index: 1;
+    opacity: 0.65;
+    transform: translateX(-0.2rem) scale(0.5);
+  }
+
+  &::after {
+    width: 3.3rem;
+    height: 3.3rem;
+    z-index: 0;
+    opacity: 0.35;
+    transform: translateX(-0.35rem) scale(0.5);
+  }
+
+  & ${UserAvatar} {
+    transition: box-shadow 0.3s ease-in-out;
+  }
+
+  &.active ${UserAvatar}, &:hover ${UserAvatar} {
+    box-shadow: none;
+  }
+
+  &.active::before,
+  &:hover::before {
+    transform: translateX(-0.2rem);
+  }
+
+  &.active::after,
+  &:hover::after {
+    transform: translateX(-0.35rem);
+  }
+
+  @keyframes pulseInner {
+    from {
+      transform: translateX(-0.2rem) scale(0);
+    }
+
+    to {
+      transform: translateX(-0.2rem);
+    }
+  }
+
+  @keyframes pulseOuter {
+    from {
+      transform: translateX(-0.35rem) scale(0);
+    }
+
+    to {
+      transform: translateX(-0.35rem);
+    }
+  }
 
   @media ${breaks.AppMinNavPoint} {
     margin-left: initial;
@@ -22,18 +99,6 @@ const StyledUser = styled(NavLink)`
       display: none;
     }
   }
-`;
-
-const UserAvatar = styled.img`
-  display: block;
-  width: 4rem;
-  width: 3.6rem;
-  aspect-ratio: 1;
-  object-fit: cover;
-  object-position: center;
-  border-radius: 50%;
-  outline: 2px solid var(--color-grey-100);
-  transition: outline 0.3s ease-in-out;
 `;
 
 const StyledLoaderContainer = styled.div`
