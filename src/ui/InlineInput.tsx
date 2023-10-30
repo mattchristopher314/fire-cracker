4a0be68dab2d81a01d7d7d91da661d5e1ca52e7b
mattchristopher314 <89071215+mattchristopher314@users.noreply.github.com>
2023-10-30 11:28:38 +0000

    Fix inline input display on mobile devices

--- src/ui/InlineInput.tsx
+++ src/ui/InlineInput.tsx
@@ -2,12 +2,13 @@ import { styled } from "styled-components";
 
 const InlineInput = styled.input`
   padding: 0 !important;
+  outline: none;
+  border-radius: 0;
   border: none;
   border-bottom: 1px solid var(--color-slate-400);
   padding: 0.8rem 1.2rem;
 
   &:focus-visible {
-    outline: none;
     border-bottom: 1px solid var(--color-brand-lo);
   }
 
